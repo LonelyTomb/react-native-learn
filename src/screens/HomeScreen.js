be0dd9aa1fcf4c6a5e1bce287fce66ac6fc20b8b
VIctory <garubav@gmail.com>
2021-02-01 00:07:34 +0100

    create Buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,33 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScroon</Text>;
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.text}>Hello</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => {
+          console.log("Pressed!");
+        }}
+        style={styles.buttonStyle}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
+  viewStyle: {
+    marginVertical: 10,
+    marginHorizontal: 15,
+  },
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+    marginTop: 15,
+  },
+  buttonStyle: {
+    marginVertical: 50,
+  },
 });
 
 export default HomeScreen;
